1d27996c37cb64c69599da4de0b681fcaecabc58
Welcome You <fake1234755@gmail.com>
2025-04-19 16:06:55 +0300

    add graphiks

--- src/components/DolphinAnimation.jsx
+++ src/components/DolphinAnimation.jsx
@@ -4,31 +4,28 @@ import { motion } from 'framer-motion';
 const DolphinAnimation = () => {
   return (
     <motion.div
-      className="absolute w-18 md:w-36 lg:w-48 z-10"
-      initial={{
-        x: Math.random() * window.innerWidth - 100, // Случайная начальная позиция по X
-        y: Math.random() * window.innerHeight - 100, // Случайная начальная позиция по Y
-        rotate: 0,
-      }}
-      animate={{
-        x: Math.random() * window.innerWidth - 100, // Случайная конечная позиция по X
-        y: Math.random() * window.innerHeight - 100, // Случайная конечная позиция по Y
-        rotate: 360, // Полный оборот вокруг своей оси
+      className="absolute w-24 md:w-36 lg:w-48 z-1"
+      initial={{ x: '800%', rotate: 0 }} // Начальная позиция за правым краем экрана
+      animate={{ 
+        x: '-100%', // Конечная позиция за левым краем экрана
+        rotate: 360 // Полный оборот вокруг своей оси
       }}
       transition={{
-        duration: 5, // Длительность анимации
+        duration: 8, // Длительность анимации (время прохода через экран)
         repeat: Infinity, // Бесконечное повторение
-        ease: 'linear', // Линейное движение
+        ease: 'linear' // Линейное движение
       }}
     >
       {/* Изображение дельфина */}
-      <img
-        src="/dolphin.png" // Убедитесь, что файл с изображением дельфина находится в папке public
-        alt="Flying Dolphin Animation"
-        className="w-full"
+      <img 
+        src="/logo1.png" // Убедитесь, что файл с изображением дельфина находится в папке public
+        alt="Dolphin Animation" 
+        className="w-full z-10" 
       />
     </motion.div>
   );
 };
+
+
 
 export default DolphinAnimation;
